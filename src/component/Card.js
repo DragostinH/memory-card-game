736839de0370096351183d8b2c1ea4996a77fd27
DragostinH <dragostin.hristov@gmail.com>
2022-06-12 00:00:33 +0300

    fix:aligned cards on stage 3 to fit screen

--- src/component/Card.js
+++ src/component/Card.js
@@ -4,18 +4,27 @@ import "../index.css";
 
 export default function Card(props) {
 
-    const { image, name, status, handleCardClick,  } = props;
+    const { image, name, status, handleCardClick, } = props;
+
+    let charName = name.split(' ');
 
     return (
-        <div onClick={handleCardClick} className="card border-2 cursor-pointer border-secondary-500 rounded-lg transform transition-all 
+        <div onClick={handleCardClick} className="card border-2 max-w-[304px] cursor-pointer border-secondary-500 rounded-lg transform transition-all 
         hover:scale-105 animate-popping-in">
 
             <img className="rounded-t-lg" src={image} alt="card" />
             <div className="name-and-status-container flex flex-col">
-                {status === "Alive" ?
-                    <p className="text-secondary-500 uppercase bg-primary-900 rounded-b-lg text-center">{status}</p>
+                {charName.length > 1 ?
+                    <h2 className="text-center text-[0.6rem] text-secondary-500 font-bold">{charName[1]}</h2>
                     :
-                    <p className="text-[rgb(255,255,255)] uppercase text-center bg-secondary-900 rounded-b-lg">{status}</p>}
+                    <p className="text-center text-secondary-500 text-xs font-bold">{charName[0]}</p>
+                }
+                {
+                    status === "Alive" ?
+                        <p className="text-secondary-500 text-sm uppercase bg-primary-900 rounded-b-lg text-center">{status}</p>
+                        :
+                        <p className="text-[rgb(255,255,255)] text-sm uppercase text-center bg-secondary-900 rounded-b-lg">{status}</p>
+                }
             </div>
         </div>
     );
--- src/component/Header.js
+++ src/component/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Header() {
     return (
         <header className="bg-primary-900 border-b-2 flex justify-center items-center text-secondary-400">
-            <h1 className='text-secondary-400 uppercase font-semibold text-xl'>Rick & Morty Memory Card Game</h1>
+            <h1 className='text-secondary-400 text-xl'>Rick & Morty Memory Card Game</h1>
         </header>
     )
 }
